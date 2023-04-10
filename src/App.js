7b4f5bd67a1ddbc8cd676876ff4d71dbf4058f87
Rishi-Kant <kant3397@gmail.com>
2023-04-10 23:32:20 +0530

    onMouseOver and Mouseout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  
+
+  const[eventTime,setEventTime]=useState([])
+
+
+
+  
+function HandleMouseOver(){
+  const date=new Date()
+  const time =date.getTime()
+  setEventTime([...eventTime,time])
+ 
+   
+}
+function HandleMouseOut(){
+  const date=new Date()
+  const time =date.getTime()
+  setEventTime([...eventTime,time])
+ 
+}
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <h1 onMouseOver={HandleMouseOver} onMouseOut={HandleMouseOut}>Tracking the Date on events</h1>
+      <h2>This is h2 heading</h2>
+      <div>This is div</div>
+      <div>
+        <div>
+          <h1>Time</h1>
+       <ul>
+        {
+          eventTime.map((data,index)=><li key={index}>{data}</li>)
+        }
+       </ul>
+       </div>
+       <div>
+      
+       </div>
+       </div>
     </div>
   );
 }
